0c9b88a16f230acc12015df88f7eaa89c4067c4c
Yana <yanaegorova69.3@gmail.com>
2021-09-23 23:25:22 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,152 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Nose Clip',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: '100% brand new and high quality.',
+            },
+            {
+                text: 'Adjustable stainless steel frame.stop water enter into your nose.',
+            },
+            {
+                text: 'Packaging including:1Pcs Nose Clip ;Used by most synchronized swimmers.',
+            },
+            {
+                text: 'This allows for longer underwater push-offs and more oxygen to use in your race.',
+            },
+            {
+                text: 'Non toxic,low profile fit for increased comfort and look.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Play Balls',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                title: 'Safety',
+                text: 'Made from non-toxic material, non-recycled and non-PVC plastic, BPA free, Phthalate free and lead free.',
+            },
+            {
+                title: 'Colorful',
+                text: 'Each ball measures 2. 3 inches in diameter, a perfect size for small child\'s hand. 6 bright and attractive Colors: blue, red, pink, green, orange and yellow.',
+            },
+            {
+                title: 'Quality',
+                text: 'Crush-proof balls are designed to withstand ~100lbs that is strong enough for adult and soft enough for children.',
+            },
+            {
+                title: 'Organize',
+                text: 'Strong and reusable mesh bag for re-storing the balls when not in Use. Great for filling any ball pit, tent, playhouse, kiddie pool, playpen and bounce house.',
+            },
+        ],
+        images: ['./img/img2-1.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Bean Bag',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                title: 'A Touch Of Personality',
+                text: 'Easily customize your living room or bedroom with this bold bean bag. With an unbeatable combination of soft textures and impeccable style, this accessory will be sure to enliven any lounge space.',
+            },
+            {
+                title: 'Modern Design',
+                text: 'With a cozy, round design, our bean bag is the ideal modern accessory for your interior space. Finished with a smooth, solid color, this accessory not only offers a sophisticated look but also provides incredible comfort.',
+            },
+            {
+                title: 'Removable Covers With Safety Zippers',
+                text: 'You can easily remove the bean bag cover for cleaning purposes after a long period of use. Just use the childproof safety zippers to unfasten the skin and slip out the inner filling. This can be machine washed.',
+            },
+            {
+                title: 'Foam Filling',
+                text: 'Our super soft bean bags are filled with small chunks of high-density foam, which helps retain shape and provides amazing support. With its durable nature, this foam is made to last longer and softer. The filling comes in an inner liner, so you don\'t have to worry about any mess.',
+            },
+            {
+                title: 'Dimensions',
+                text: 'Choose an accessory that is the perfect size for you and your furniture. This bean bag measures to be 34.00” W x 34.00” D x 25.00” H. You will love how much your space can transform with the simple addition of this charming bean bag.',
+            },
+        ],
+        images: ['./img/img3-1.jpg', './img/img3-2.jpg', './img/img3-3.png', './img/img3-4.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Children Bedside Bassinet',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                title: 'Keeps Baby Close',
+                text: 'This timeless baby bassinet with solid wood base and legs keeps baby safely by your bedside | Neutral design is ideal for boys or girls | Made of sustainable New Zealand pine wood.',
+            },
+            {
+                title: 'Airflow Mesh',
+                text: 'Breathable mesh on sides promote airflow and cooler sleep environment for baby.',
+            },
+            {
+                title: 'Tons Of Storage',
+                text: 'Large storage shelf under the frame keeps baby’s necessities within reach.',
+            },
+            {
+                title: 'Size',
+                text: 'Assembled dimensions: 33”L x 20.5”W x 32“H | Bassinet includes 1-inch waterproof mattress pad and fitted bassinet sheet.',
+            },
+            {
+                title: 'We Put Your Baby\'S Safety First',
+                text: 'Certified to meet or exceed all safety standards set by the CPSC and ASTM | We know chemicals have no place in your nursery, so we use a non-toxic multi-step painting process that is lead and phthalate safe.',
+            },
+         
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.png', './img/img4-6.png', './img/img4-7.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Adjustable Punch',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Antimicrobial agent prevents growth of bacteria on the surface of the punch.',
+            },
+            {
+                text: 'Adjusts to punch 2 to 3 9/32-Inch holes.',
+            },
+            {
+                text: 'Easy to remove chip tray.',
+            },
+            {
+                text: 'Hollow punch mechanism assures a clean punch through of up to 160 sheets.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png', './img/img5-5.png', './img/img5-6.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Long Barrel Styler',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Hair Type: Normal',
+            },
+            {
+                text: 'Size: 1.25 Inch (Pack of 1)',
+            },
+            {
+                text: 'Color: Black',
+            },
+            {
+                text: 'Item Dimensions: 18.5 x 4.75 x 4 inches',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png', './img/img6-3.png', './img/img6-4.png', './img/img6-5.png'],
+    },
+]
